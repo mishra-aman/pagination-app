a0acb14163c57f0e98658fc549d36d79181199d4
Aman Mishra <sharda.aman.mishra@gmail.com>
2021-07-15 00:08:58 +0530

    publish

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <pagination msg="Hello"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import pagination from './components/pagination.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pagination
   }
 }
+ 
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+
